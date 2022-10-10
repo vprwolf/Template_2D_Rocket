cf5dff0df0d9b1d152b91fafaf175c01e77c9c45
vprwolf <morry0126@naver.com>
2022-10-10 23:37:25 +0900

    week2_2

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -9,6 +9,7 @@ public class gameManager : MonoBehaviour
 {
     public GameObject camera;
     public Rigidbody2D train;
+    public GameObject dummy;
     public GameObject appearingTrail;
     public GameObject ButtonForward;
     public static gameManager I;
@@ -16,10 +17,6 @@ public class gameManager : MonoBehaviour
     public float pos = 0.0f;
     public Transform target01;
     public Transform target02;
-    // public float speed = 0.05f;
-
-    public Transform movetarget;
-    public float Speed = 1f;
 
 
 
@@ -47,7 +44,8 @@ public class gameManager : MonoBehaviour
 
     public void moveForward()
     {
-        Invoke("OnInvoke", 0.5f); 
+        dummy.transform.Translate(0f, 1.2f, 0f);
+        Invoke("OnInvoke", 0.5f);
     }
 
 
@@ -57,13 +55,9 @@ public class gameManager : MonoBehaviour
         //Vector3 endPos = new Vector3(0,1.2f,0);
 
         //train.transform.position += new Vector3(0f, 1.2f, 0f);
-        //train.transform.position = Vector3.Lerp(spotPos, new Vector3(0f,spotPos.y+1.2f, 0f), 0.5f);
-        //train.transform.position = Vector3.Lerp(spotPos, spotPos+endPos, 0.5f);
 
         //train.transform.Translate(0f, 1.2f, 0f);
-        //train.MovePosition(transform.position - transform.up*Time.deltaTime* frwaySpeed);
-        //transform.position = Vector3.MoveTowards(transform.position, movetarget.position, Speed * Time.deltaTime);
-        train.transform.Translate(0f, 1.2f, 0f);
+        train.transform.position = Vector3.Lerp(train.transform.position, dummy.transform.position, 0.05f);
 
     }
 
